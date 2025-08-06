78d0b62b3f70aff304ae6e608eef30745fa60626
XayrullayevRaxmatillo <xayrullayevrahmatilloh@gmail.com>
2025-08-06 15:20:04 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <h1>Hayrullayev Rahamtillo</h1>
+      <h2>kimdurla haqida !!!</h2>
+      <ul>
+        <li>CSS</li>
+        <li>CSS</li>
+        <li>CSS</li>
+        <li>CSS</li>
+        <li>CSS</li>
+      </ul>
+      <h2>nima yozishi bilmadim</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tempore voluptates eos suscipit iste <br />
+         excepturi blanditiis asperiores maiores assumenda dolores necessitatibus, consequuntur velit ducimus <br />
+         ut quam recusandae pariatur autem animi dolorum corporis at odit quod. Laudantium, minus. Dolor fuga <br />
+         ducimus sed numquam nemo animi modi. Sit dolorum itaque rerum sunt quod cum ratione, assumenda tempore <br />
+         minima doloribus nihil maxime dignissimos aspernatur culpa harum explicabo unde officiis perferendis minus <br />
+         fugit. Doloremque labore non deleniti eos laboriosam. Amet modi corrupti veniam, dolores molestias illum beatae <br />
+         sunt, consequatur quod aut similique natus non tempore, quis unde commodi! Tempora quod consectetur neque, 
+         </p>
+      <h2>nima yozishi bilmadim</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tempore voluptates eos suscipit iste <br />
+         excepturi blanditiis asperiores maiores assumenda dolores necessitatibus, consequuntur velit ducimus <br />
+         ut quam recusandae pariatur autem animi dolorum corporis at odit quod. Laudantium, minus. Dolor fuga <br />
+         ducimus sed numquam nemo animi modi. Sit dolorum itaque rerum sunt quod cum ratione, assumenda tempore <br />
+         minima doloribus nihil maxime dignissimos aspernatur culpa harum explicabo unde officiis perferendis minus <br />
+         fugit. Doloremque labore non deleniti eos laboriosam. Amet modi corrupti veniam, dolores molestias illum beatae <br />
+         sunt, consequatur quod aut similique natus non tempore, quis unde commodi! Tempora quod consectetur neque, 
+         </p>
+    </div>
+  )
+}
+
+export default App
+
+
